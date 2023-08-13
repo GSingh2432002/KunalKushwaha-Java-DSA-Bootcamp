2493fae0966718b98b38b5672cc4bdcebf06acc3
GSingh2432002 <gsingh332211@gmail.com>
2023-08-13 22:59:07 +0530

    Update

---
+++ Java53.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Java53
+{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter principle amount");
+        double principleAmount = obj.nextDouble();
+        System.out.println("Enter the interest rate ");
+        double InterestRate = obj.nextDouble();
+        System.out.println("Enter the number of year ");
+        double year = obj.nextDouble();
+        double FV = principleAmount * (1+((InterestRate*year)/100));
+        System.out.println("FV Amount: " + FV);
+    }
+}
