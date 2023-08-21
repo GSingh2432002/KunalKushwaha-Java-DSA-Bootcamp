6a349a9b2745302f0dfe6e088b2805f12008a7ed
GSingh2432002 <gsingh332211@gmail.com>
2023-08-21 13:02:41 +0530

    Update

--- Java69.java
+++ Java69.java
@@ -1,79 +1,49 @@
 //Search in Rotated Sorted Array
 import java.util.*;
+import java.io.*;
 class Java69
 {
     public static void main(String[] args) {
-        int[] arr = {4,5,6,7,0,1,2,3};
-        System.out.println(findPivot(arr));
+        int[] arr = {5,6,7,8,9,10,1,2,3};
+        int target = 9;
+        int ans = find(arr,target);
+        System.out.println(ans);
     }
-
-    static int search(int[] nums, int target)
-    {
-        int pivot = findPivot(nums);
-
-        //If you did not find a pivot, it means the array is not rotated
-        if(pivot == -1)
-        {
-            //Just do normal binary search
-            return binarySearch(nums,target,0,nums.length - 1);
-        }
-
-        //If pivot is found, you have found 2 asc sorted arrays
-        if(target >= nums[0])
-        {
-            return binarySearch(nums, target,0,pivot - 1);
-        }
-
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
-    }
-    
-    static int binarySearch(int[] arr, int target, int start, int end)
-    {
-        while(start <= end)
-        {
-            //find middle element
-            //int mid = (start + end) / 2;
-            int mid = start + (end - start) / 2;
-
-            if(target < arr[mid])
-            {
-                end = mid - 1;
-            }
-            else if(target > arr[mid])
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    static int findPivot(int[] arr)
+    static int find(int[] arr, int target)
     {
         int start = 0;
-        int end = arr.length - 1; 
-        while (start <= end)
+        int end = arr.length - 1;
+
+        while(start <= end)
         {
-            int mid = start + (end - start) / 2;
-            // 4 cases over here
-            if(mid < end && arr[mid] > arr[mid + 1])
+            int mid = (start + end) / 2;
+            
+            if(arr[mid] == target)
             {
                 return mid;
             }
-            if(mid > start && arr[mid] < arr[mid - 1])
+            else if(arr[start] <= arr[mid])
             {
-                return mid - 1;
+                //start to mid part is sorted
+                if(target >= arr[start] && arr[mid] > target)
+                {
+                    end = mid - 1;
+                }
+                else 
+                {
+                    start = mid + 1;
+                }
             }
-            if(arr[mid] <= arr[start])
+            else if(arr[mid] <= arr[end])
             {
-                end = mid - 1;
-            }
-            else 
-            {
-                start = mid + 1;
+                //mid to end part is sorted
+                if(target > arr[mid] && arr[end] >= target)
+                {
+                    start = mid + 1;
+                }
+                else{
+                    end = mid - 1;
+                }
             }
         }
         return -1;
