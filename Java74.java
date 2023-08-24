709fe74bacdb0367c703c8d6a92774f43462db3e
GSingh2432002 <gsingh332211@gmail.com>
2023-08-24 17:41:05 +0530

    Update

--- Java74.java
+++ Java74.java
@@ -4,10 +4,7 @@ import java.util.Arrays;
 class Java74
 {
     public static void main(String[] args) {
-       int[] arr = {1,2,3,4,5,6,7,8,9,11,13,17};
-       int n = arr.length;
-       int ans = reverseArray(arr,0,n-1);
-       System.out.println(Arrays.toString(arr));
+       
     }
     static void rotate(int[] nums, int k) {
         int n = nums.length;
@@ -17,7 +14,7 @@ class Java74
         reverseArray(nums,0,k-1);
         reverseArray(nums,k,n-1);
     }
-   static int reverseArray(int[] nums,int start, int end)
+   static void reverseArray(int[] nums,int start, int end)
     {
         for(int i=start, j=end;i<j;i++,j--)
         {
@@ -25,7 +22,7 @@ class Java74
             nums[i] = nums[j];
             nums[j] = temp;
         }
-        return reverseArray(nums, 1, end - 1);
+        reverseArray(nums, start+1, end-1);
     }
     
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,4 +1,1 @@
- int arr1[] = {1,1,1,2,2,3,3,3};
-        int arr2[] = {3,3,3,4};
-         
-        int left = 0, right = 0;
+nums
