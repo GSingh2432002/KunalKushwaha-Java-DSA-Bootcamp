9d7dddc624d98371663c5edbdb89a749f8972119
GSingh2432002 <gsingh332211@gmail.com>
2023-08-27 21:19:57 +0530

    Update

--- Java74.java
+++ Java74.java
@@ -1,40 +1,43 @@
-//Rotate Array
-//Not yet done
-import java.util.*;
-class Java74
-{
-    public static void main(String[] args) {
-        //Array to rotate
-        int[] nums = {12, -9, 111, 23, -99, 67, 98, 90, 11, 45};
-        //numbers of rotations to be performed
-        //we can change the number of rotations accordingly
-        int r = 1;
-        //determines the length of array
-        int n = nums.length;
-        System.out.println("Array before rotation: ");
-        for(int i = 0; i<n; i++)
-        {
-            System.out.print(nums[i]+ " ");
-        }
-        System.out.println();
-        rotate(nums,r,n);
-        System.out.println("Array after rotation: ");
-        for(int i=1; i<n;i++)
-        {
-            System.out.print(nums[i]+" ");
-        }
-    }
-    public static void rotate(int[] nums, int k, int n) {
-        for(int i = 0; i<k; i++)
+//Cyclic Rotated Array
+import java.util.Arrays;
+import java.util.Scanner;
 
-        reverseArray(nums,n);
+public class Java74 {
+    static void reverse(int[] nums, int low, int high) {
+        while (low < high) {
+            int temp = nums[low];
+            nums[low] = nums[high];
+            nums[high] = temp;
+            low++;
+            high--;
+        }
     }
-    public static void  reverseArray(int[] nums,int n)
-    {
-        int i, temp;
-        temp = nums[n-1];
-        for(i=n-1;i>0;i--)
-        nums[i] = nums[i-1];
-        nums[0] = temp;
+    static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - k - 1);
+        reverse(nums, n - k, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Array Size: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter Steps: ");
+        int k = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element no " + (i + 1) + "-> ");
+            arr[i] = scanner.nextInt();
+        }
+
+        rotate(arr, k);
+        
+        System.out.print("Rotated Array -> [ ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("]");
     }
 }
