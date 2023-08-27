85e08054ec89ba29f8d197380c0f8bcf5da96e74
GSingh2432002 <gsingh332211@gmail.com>
2023-08-27 18:16:46 +0530

    Update

--- Java74.java
+++ Java74.java
@@ -1,20 +1,40 @@
 //Rotate Array
-class Solution {
-    public void rotate(int[] nums, int k) {
+//Not yet done
+import java.util.*;
+class Java74
+{
+    public static void main(String[] args) {
+        //Array to rotate
+        int[] nums = {12, -9, 111, 23, -99, 67, 98, 90, 11, 45};
+        //numbers of rotations to be performed
+        //we can change the number of rotations accordingly
+        int r = 1;
+        //determines the length of array
         int n = nums.length;
-        k = k%n;
-
-        reverseArray(nums,0,n-1);
-        reverseArray(nums,0,k-1);
-        reverseArray(nums,k,n-1);
-    }
-   void reverseArray(int[] nums,int start, int end)
-    {
-        for(int i=start, j=end;i<j;i++,j--)
+        System.out.println("Array before rotation: ");
+        for(int i = 0; i<n; i++)
         {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+            System.out.print(nums[i]+ " ");
         }
+        System.out.println();
+        rotate(nums,r,n);
+        System.out.println("Array after rotation: ");
+        for(int i=1; i<n;i++)
+        {
+            System.out.print(nums[i]+" ");
+        }
+    }
+    public static void rotate(int[] nums, int k, int n) {
+        for(int i = 0; i<k; i++)
+
+        reverseArray(nums,n);
+    }
+    public static void  reverseArray(int[] nums,int n)
+    {
+        int i, temp;
+        temp = nums[n-1];
+        for(i=n-1;i>0;i--)
+        nums[i] = nums[i-1];
+        nums[0] = temp;
     }
 }
