50e1ef93b7826abac8ca4f1a4e56f3a543905e87
GSingh2432002 <gsingh332211@gmail.com>
2023-09-22 22:41:12 +0530

    Update

---
+++ Java110.java
@@ -0,0 +1,53 @@
+// /Rotate Image
+class Java110
+{
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("Rotated Matrix:");
+        printMatrix(matrix);
+    }
+    public static void rotate(int[][] matrix)
+    {
+        int n = matrix.length;
+
+        // Transpose the matrix (swap rows with columns)
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
