c9e9c5ca1e3c76634af0b84aef22109fc2cddbdc
GSingh2432002 <gsingh332211@gmail.com>
2023-10-15 21:59:12 +0530

    Update

---
+++ Java140.java
@@ -0,0 +1,31 @@
+//Longest common subsequence
+import java.util.*;
+class Java140
+{
+    public static void main(String[] args) {
+        String text1 =  "abcde";
+        String text2 =  "ace";
+        int resultString = lcs(text1, text2);
+        System.out.println("The longest common substring is: "+resultString );
+    }
+    public static int lcs(String text1, String text2)
+    {
+        int[][] dp = new int[text1.length()+1][text2.length()+1];
+
+        for(int i=1;i<dp.length;i++)
+        {
+            for(int j=1;j<dp[0].length;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                {
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[text1.length()][text2.length()];
+    }
+}
